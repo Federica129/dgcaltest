d3e903e1eb5a277e092d25261944b2c477f7ce83
Federica129 <schillacifederica99@gmail.com>
2022-12-13 18:44:42 +0100

    fix: add destruct in card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,28 +1,32 @@
 import styles from "./index.module.scss";
 
 const Card = ({ info }) => {
+  const { title, name, photo, price } = info[0];
+  const { rate, olfatto, color, ep, efferv } = info[0].reviews[0];
+  const { body } = info[0]?.comments[0];
+
   return (
     <>
       <div className={styles.bg}>
         <div className={styles.Card}>
           <div
             className={styles.photo}
-            style={{ backgroundImage: `url(${info[0]?.photo})` }}
+            style={{ backgroundImage: `url(${photo})` }}
           ></div>
           <div className={styles.info}>
             <div className={styles.title}>
-              <h1>{info[0]?.title}</h1>
+              <h1>{title}</h1>
               <div className={styles.info2}>
-                <p>{info[0]?.name}</p>{" "}
+                <p>{name}</p>
                 <p>
                   <span>Voto </span>
-                  {info[0]?.reviews[0]?.rate}/10
+                  {rate}/10
                 </p>
               </div>
             </div>
             <div className={styles.price}>
               <h1>Prezzo</h1>
-              <p>{info[0]?.price}</p>
+              <p>{price}</p>
             </div>
             <div className={styles.addBtn}>
               <button>Aggiungi</button>
@@ -32,24 +36,24 @@ const Card = ({ info }) => {
         <div className={styles.review}>
           <h1>Recensioni</h1>
           <div>
-            <p>Olfatto: </p>
-            <p>{info[0]?.reviews[0]?.olfatto}/10 </p>
+            <p>Olfatto:</p>
+            <p>{olfatto}/10</p>
           </div>
           <div>
-            <p>Colore: </p>
-            <p>{info[0]?.reviews[0]?.color}/10 </p>
+            <p>Colore:</p>
+            <p>{color}/10</p>
           </div>
           <div>
-            <p>Experience / Prezzo: </p>
-            <p>{info[0]?.reviews[0]?.ep}/10 </p>
+            <p>Experience / Prezzo:</p>
+            <p>{ep}/10</p>
           </div>
           <div>
             <p>Effervescienza: </p>
-            <p>{info[0]?.reviews[0]?.efferv}/10 </p>
+            <p>{efferv}/10</p>
           </div>
           <div>
             <p>Commento: </p>
-            <p>{info[0]?.comments[0]?.body}</p>{" "}
+            <p>{body}</p>
           </div>
         </div>
       </div>
